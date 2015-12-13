0399d354e6acb09c5e7b3df40ff97c11ec28af96
Administrator <Administrator@WIN-7AC6L01SN90>
2015-12-13 18:26:54 +0800

    text github

---
+++ src/com/example/zzzz01/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.zzzz01;
+
+import me.maxwin.view.XListView;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	private XListView mListView;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
